a3f0adcbc8c6c5bb309627a23cd80768255daab9
OlexandrYakovenko <olexandr.yakov@gmail.com>
2017-10-12 20:13:05 +0300

    Simple navigation bar with flexbox

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,11 @@ jade = require('gulp-jade');
 
 gulp.task('templates', function() {
  
-  gulp.src('templ/*.jade')
+  gulp.src('templ/menu.jade')
     .pipe(jade({
       pretty: true   
     }))
-    .pipe(gulp.dest(''))
+    .pipe(gulp.dest('example-1'))
     .pipe(notify({ message: 'Your Jade file has been molded into HTML.' }))    
 });
 
@@ -18,7 +18,7 @@ gulp.task('webserver', function() {
   gulp.src('')
     .pipe(server({
       livereload: true,
-      defaultFile: 'index.html',
+      defaultFile: 'example-1/menu.html',
       directoryListing: false,
       open: false
     }));
