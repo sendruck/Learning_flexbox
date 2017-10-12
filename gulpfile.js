d5f4b9a661a5bf817c62ab247fea0f22c230833a
OlexandrYakovenko <olexandr.yakov@gmail.com>
2017-10-12 19:02:58 +0300

    Added Jade template, and set the flex container

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp'),
-server = require('gulp-server-livereload');
+server = require('gulp-server-livereload'),
+ notify = require('gulp-notify'),
+jade = require('gulp-jade');
+
+
+gulp.task('templates', function() {
+ 
+  gulp.src('templ/*.jade')
+    .pipe(jade({
+      pretty: true   
+    }))
+    .pipe(gulp.dest(''))
+    .pipe(notify({ message: 'Your Jade file has been molded into HTML.' }))    
+});
 
 gulp.task('webserver', function() {
   gulp.src('')
@@ -11,6 +24,10 @@ gulp.task('webserver', function() {
     }));
 });
 
+gulp.task('watch', function() {
+  gulp.watch('*.jade', ['templates']);
+  }); 
 
 
- gulp.task('default', ['webserver']);  
+
+ gulp.task('default', ['templates', 'watch','webserver']);  
