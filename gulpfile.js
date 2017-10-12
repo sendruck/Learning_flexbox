b10c12436fd997cb6d28bda58757b5226c8d2902
OlexandrYakovenko <olexandr.yakov@gmail.com>
2017-10-12 20:33:28 +0300

    Created nested menu with flexbox

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,11 @@ jade = require('gulp-jade');
 
 gulp.task('templates', function() {
  
-  gulp.src('templ/menu.jade')
+  gulp.src('templ/nested-menu.jade')
     .pipe(jade({
       pretty: true   
     }))
-    .pipe(gulp.dest('example-1'))
+    .pipe(gulp.dest('example-2'))
     .pipe(notify({ message: 'Your Jade file has been molded into HTML.' }))    
 });
 
@@ -18,7 +18,7 @@ gulp.task('webserver', function() {
   gulp.src('')
     .pipe(server({
       livereload: true,
-      defaultFile: 'example-1/menu.html',
+      defaultFile: 'example-2/nested-menu.html',
       directoryListing: false,
       open: false
     }));
